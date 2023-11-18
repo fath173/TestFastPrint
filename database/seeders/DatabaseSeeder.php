ba2bd92e82bac98e630d0df210c98e781bf7f709
Fathor Rahman <fathorrahman2357@gmail.com>
2023-11-18 20:13:58 +0700

    add

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             KategoriSeeder::class,
             StatusSeeder::class,
-            ProdukSeeder::class,
         ]);
     }
 }
